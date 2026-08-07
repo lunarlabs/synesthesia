ce5b9483181133f6c87df485384137ff9d64dff3
Matthias <aussieevil@hotmail.com>
2026-08-07 01:55:05 -0700

    I'm nuts

---
+++ addons/synesthesia_road_editor/synesthesia_road_editor.gd
@@ -0,0 +1,34 @@
+@tool
+extends EditorPlugin
+
+
+func _enable_plugin() -> void:
+	# Add autoloads here.
+	pass
+
+
+func _disable_plugin() -> void:
+	# Remove autoloads here.
+	pass
+
+
+func _enter_tree() -> void:
+	# Initialization of the plugin goes here.
+	pass
+
+
+func _exit_tree() -> void:
+	# Clean-up of the plugin goes here.
+	pass
+
+func _has_main_screen() -> bool:
+	return true
+
+func _make_visible(visible: bool) -> void:
+	pass
+	
+func _get_plugin_name() -> String:
+	return "SynRoad"
+
+func _get_plugin_icon() -> Texture2D:
+	return EditorInterface.get_editor_theme().get_icon("Node", "EditorIcons")
